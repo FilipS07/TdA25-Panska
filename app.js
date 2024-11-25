14c9f96b4584b2d5b22069a021f8459d577f532e
FilipS07 <89216510+FilipS07@users.noreply.github.com>
2024-11-25 20:08:48 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');  
+const path = require('path');        
+const app = express();               
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/api', (req, res) => {
+    res.json({ organization: 'Student Cyber Games' });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server běží na http://localhost:${PORT}`);
+});
