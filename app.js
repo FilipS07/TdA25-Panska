c13e9cbc89f04caa7772329acf57d92313d2f8b5
FilipS07 <89216510+FilipS07@users.noreply.github.com>
2024-11-25 23:05:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'Public/index.html'));
 });
 
 app.get('/api', (req, res) => {
